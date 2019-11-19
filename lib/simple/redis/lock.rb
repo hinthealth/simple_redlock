9864a213879ce2696d357eecdc9e3926276906d8
MaicolBen <maicol.bentancor@gmail.com>
2019-11-19 11:54:27 -0300

    Create gem from seed

---
+++ lib/simple/redis/lock.rb
@@ -0,0 +1,10 @@
+require "simple/redis/lock/version"
+
+module Simple
+  module Redis
+    module Lock
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/simple/redis/lock/version.rb
@@ -0,0 +1,7 @@
+module Simple
+  module Redis
+    module Lock
+      VERSION = "0.1.0"
+    end
+  end
+end
