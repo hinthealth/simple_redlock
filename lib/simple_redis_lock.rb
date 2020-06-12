30dc55f8f1492fcf62d4e29befb60b8fe764c8df
MaicolBen <maicol.bentancor@gmail.com>
2020-06-12 17:18:03 -0300

    Include memory extension as last file

--- lib/simple_redis_lock.rb
+++ lib/simple_redis_lock.rb
@@ -3,9 +3,9 @@
 LockError = Class.new(StandardError)
 
 require 'active_support/core_ext/numeric/time'
-require_relative 'memory_extension'
 require_relative 'simple_redis_lock/locker'
 require_relative 'simple_redis_lock/lockable'
+require_relative 'memory_extension'
 
 module SimpleRedisLock
 end
