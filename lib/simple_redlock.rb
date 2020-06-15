661c1448f88178dbcea1ccc8c5856ce2a12ec0da
Maicol Bentancor <maicol.bentancor@gmail.com>
2020-06-15 12:03:54 -0300

    Make redis url configurable (#18)

--- lib/simple_redlock.rb
+++ lib/simple_redlock.rb
@@ -3,4 +3,11 @@ require_relative 'simple_redlock/locker'
 require_relative 'simple_redlock/lockable'
 
 module SimpleRedlock
+  class << self
+    mattr_accessor :redis_url
+  end
+
+  def self.configure(&block)
+    yield self
+  end
 end
